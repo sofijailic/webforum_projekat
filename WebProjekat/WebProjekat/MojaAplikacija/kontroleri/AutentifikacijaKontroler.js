439524e6d002fadea90a96b00cba01653f621089
Sofija Ilic <sofijailic505@gmail.com>
2017-07-25 21:07:57 +0200

    uradjen kompletan login

--- WebProjekat/WebProjekat/MojaAplikacija/kontroleri/AutentifikacijaKontroler.js
+++ WebProjekat/WebProjekat/MojaAplikacija/kontroleri/AutentifikacijaKontroler.js
@@ -1,4 +1,4 @@
-﻿forum.controller('AutentifikacijaKontroler', function ($scope,AutentifikacijaFabrika,$window) {
+﻿forum.controller('AutentifikacijaKontroler', function ($scope,AutentifikacijaFabrika,$window,$rootScope) {
     
     function inicijalizacija() {
         console.log('Inicijalizovan autentifikacija kontroler');
@@ -22,7 +22,29 @@
     $scope.Login = function (korisnik) {
 
         AutentifikacijaFabrika.LogovanjeKorisnika(korisnik).then(function (odgovor) {
-            console.log(odgovor.data);
+            if (odgovor.data == null) {
+
+                alert('Korisnik sa tim korisnickim imenom i lozinkom ne postoji');
+            } else {
+
+                console.log(odgovor);
+                document.cookie = "korisnik=" + JSON.stringify({
+                    username: odgovor.data.KorisnickoIme,
+                    uloga: odgovor.data.Uloga,
+                    imePrezime: odgovor.data.Ime + " " + odgovor.data.Prezime
+                }) + ";expires=Thu, 01 Jan 2019 00:00:01 GMT;";
+                sessionStorage.setItem("korisnickoIme", odgovor.data.KorisnickoIme);
+                sessionStorage.setItem("uloga", odgovor.data.Uloga);
+                sessionStorage.setItem("imePrezime", odgovor.data.Ime + " " + odgovor.data.Prezime);
+
+                $rootScope.ulogovan = true;
+                $rootScope.korisnik = {
+                    username: sessionStorage.getItem("username"),
+                    uloga: sessionStorage.getItem("uloga"),
+                    imePrezime: sessionStorage.getItem("imePrezime")
+                };
+                $window.location.href = "#!/";
+            }
 
         })
     }
--- WebProjekat/WebProjekat/MojaAplikacija/kontroleri/GlavniKontroler.js
+++ WebProjekat/WebProjekat/MojaAplikacija/kontroleri/GlavniKontroler.js
@@ -1,3 +1,33 @@
 ﻿forum.controller('GlavniKontroler', function ($scope) {
-    $scope.nesto = "asdasdasd";
+    if (document.cookie !== "") {
+        //console.log(document.cookie);
+        var cookieInfo = document.cookie.substring(9, document.cookie.length);
+        var parsed = JSON.parse(cookieInfo);
+
+        sessionStorage.setItem("username", parsed.username);
+        sessionStorage.setItem("uloga", parsed.uloga);
+        sessionStorage.setItem("imePrezime", parsed.imePrezime);
+
+        $rootScope.ulogovan = true;
+        $rootScope.korisnik = {
+            username: sessionStorage.getItem("username"),
+            uloga: sessionStorage.getItem("uloga"),
+            imePrezime: sessionStorage.getItem("imePrezime")
+        };
+    } else {
+        $rootScope.ulogovan = false;
+    }
+
+    $scope.checkActive = function (path) {
+        return ($location.path().substr(0, path.length) === path) ? 'active' : '';
+    }
+
+
+    $scope.Logout = function () {
+        document.cookie = 'korisnik=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        sessionStorage.clear();
+        $rootScope.ulogovan = false;
+        $rootScope.korisnik = {};
+        document.location.href = "#!/login";
+    }
 });
